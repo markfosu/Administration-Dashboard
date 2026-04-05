8a78a35211b83c30e894bba3c1eae20e635734a5
markfosu <fosumark2@gmail.com>
2026-04-05 06:52:05 +0000

    eslint_update

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,40 @@ const js = require('@eslint/js');
 const globals = require('globals');
 
 module.exports = [
-  js.configs.recommended,
+  // Ignore ALL vendor/static files first
   {
-    files: ['**/*.js'],
-    ignores: ['node_modules/**', 'public/jquery-ui-datepicker/**'],
+    ignores: [
+      'node_modules/**',
+      'public/js/**/*.min.js',
+      'public/jquery-ui-datepicker/**',
+      'public/js/jquery-*.js',
+      'public/js/bootstrap*.js',
+      'public/js/Chart*.js',
+      'public/js/fullcalendar*.js',
+      'public/js/moment*.js'
+    ]
+  },
+
+  js.configs.recommended,
+
+  // Only lint YOUR code (not vendor code)
+  {
+    files: [
+      'server.js',
+      'test/**/*.js',
+      'public/js/tooplate-scripts.js',
+      'public/js/utils.js'
+    ],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'commonjs',
       globals: {
         ...globals.node,
-        ...globals.jest
+        ...globals.jest,
+        window: 'readonly',
+        document: 'readonly',
+        $: 'readonly',
+        Chart: 'readonly'
       }
     },
     rules: {
